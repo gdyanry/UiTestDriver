81e04fdc199f13680673500e80ceceb89863c99a
gdyanry <569161256@qq.com>
2018-08-23 08:00:43 +0800

    #

--- Sample/src/main/java/com/yanry/driver/mobile/sample/listview/EditPage.java
+++ Sample/src/main/java/com/yanry/driver/mobile/sample/listview/EditPage.java
@@ -2,10 +2,13 @@ package com.yanry.driver.mobile.sample.listview;
 
 import com.yanry.driver.core.model.base.Graph;
 import com.yanry.driver.core.model.expectation.Timing;
+import com.yanry.driver.core.model.state.ValueEquals;
 import com.yanry.driver.mobile.action.Click;
 import com.yanry.driver.mobile.property.EditableText;
 import com.yanry.driver.mobile.property.TextValidity;
+import com.yanry.driver.mobile.sample.listview.MainPage.MainListItem;
 import com.yanry.driver.mobile.view.View;
+import com.yanry.driver.mobile.view.listview.ListView;
 import com.yanry.driver.mobile.view.selector.ById;
 import com.yanry.driver.mobile.window.Window;
 import com.yanry.driver.mobile.window.WindowManager;
@@ -26,7 +29,8 @@ public class EditPage extends Window {
         EditableText etRemark = new EditableText(getViewById("et_remark"));
         Click click = new Click(new View(graph, this, new ById("finish")));
         // 添加
-        createPath(click, getWindow(MainPage.class).getListView().getSize().getShiftExpectation(Timing.IMMEDIATELY, true, true, 1))
+        ListView<MainListItem> listView = getWindow(MainPage.class).getListView();
+        createPath(click, listView.getSize().getShiftExpectation(Timing.IMMEDIATELY, true, true, 1).setTrigger(listView, new ValueEquals<>(true)))
                 .addInitState(getPreviousWindow(), getWindow(MainPage.class))
                 .addInitState(etPrincipal, true)
                 .addInitState(etBonus, true)
